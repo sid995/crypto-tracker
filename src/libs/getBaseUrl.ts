33399f3d22d03921dd13dda3cdcaebd278122f87
Siddharth Kundu <sk31395@gmail.com>
2025-05-18 01:57:04 -0400

    Refactor getBaseUrl function to ensure consistent base URL retrieval. Adjust order of checks for environment variables to prioritize VERCEL_URL while maintaining fallback to NEXT_PUBLIC_BASE_URL.

--- src/libs/getBaseUrl.ts
+++ src/libs/getBaseUrl.ts
@@ -1,9 +1,9 @@
 import { headers } from 'next/headers'
 
 export async function getBaseUrl() {
-  if (process.env.NEXT_PUBLIC_BASE_URL) return process.env.NEXT_PUBLIC_BASE_URL
-
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
+
+  if (process.env.NEXT_PUBLIC_BASE_URL) return process.env.NEXT_PUBLIC_BASE_URL
 
   const host = (await headers()).get('host')
   return host?.startsWith('http') ? host : `http://${host}`
